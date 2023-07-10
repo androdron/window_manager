65dcea2b6762a32d6d8a9e39d3e310f8b889c7a3
Andro <theandreymus@gmail.com>
2023-07-10 02:01:11 -0300

    testing

--- windows/window_manager.cpp
+++ windows/window_manager.cpp
@@ -835,10 +835,9 @@ void WindowManager::SetAlwaysOnTop(const flutter::EncodableMap& args) {
       std::get<bool>(args.at(flutter::EncodableValue("isAlwaysOnTop")));
   SetWindowPos(GetMainWindow(), isAlwaysOnTop ? HWND_TOPMOST : HWND_NOTOPMOST,
                0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-
+  HWND mainWindow = GetMainWindow();
   if (isAlwaysOnTop) {
-    HWND mainWindow = GetMainWindow();
-    ::SetWindowLong(mainWindow, GetWindowLong(mainWindow, GWL_STYLE), GetWindowLong(mainWindow, GWL_EXSTYLE) | WS_EX_NOACTIVATE | WS_EX_APPWINDOW);
+    SetWindowLong(mainWindow, GetWindowLong(mainWindow, GWL_STYLE), GetWindowLong(mainWindow, GWL_EXSTYLE) | WS_EX_NOACTIVATE | WS_EX_APPWINDOW);
   }
 }
 
